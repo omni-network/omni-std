0d3dd2e33dfb4f9c25e0a63a6dd91efce41b449b
Tyler Tarsi <hi@ttarsi.com>
2023-08-18 12:09:48 -0400

    use sol ^0.8

--- contracts/interfaces/IOmni.sol
+++ contracts/interfaces/IOmni.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8;
 
 interface IOmni {
     event TransactionSent(string indexed destChain, uint64 indexed nonce, bytes tx);
