1527f29ae179091d86fbd8e5c7d91ccdbfe6a27b
ttarsi <31632172+ttarsi@users.noreply.github.com>
2023-08-18 12:10:31 -0400

    use sol ^0.8 (#7)

--- contracts/OmniScient.sol
+++ contracts/OmniScient.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8;
 
 import {OmniCodec} from "./OmniCodec.sol";
 import {IOmni} from "./interfaces/IOmni.sol";
--- examples/counter/contracts/src/LocalCounter.sol
+++ examples/counter/contracts/src/LocalCounter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8;
 
 import {IOmniPortal} from "@omni/contracts/contracts/interfaces/IOmniPortal.sol";
 
