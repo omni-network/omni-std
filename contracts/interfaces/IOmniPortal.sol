cd63f04b93ef7dffb17d1f3b15aadeab68445524
Kevin Halliday <halliday.kevin7@gmail.com>
2023-09-05 23:18:48 -0400

    Update interfaces to match omni core contracts.

--- contracts/interfaces/IOmniPortal.sol
+++ contracts/interfaces/IOmniPortal.sol
@@ -4,17 +4,36 @@ pragma solidity ^0.8;
 import {OmniCodec} from "../OmniCodec.sol";
 
 interface IOmniPortal {
-    // txn metadata
-    function isXChainTx() external view returns (bool); // true if tx is a XChainTx
-    function txSender() external view returns (address); // tx.from if tx is from omni
-    function txSourceChain() external view returns (string memory); // tx.sourceChain if tx is from omni
-    function chain() external view returns (string memory);
-    function supportedChains(string memory) external view returns (bool);
-    function getLatestOmniBlock() external view returns (OmniCodec.Block memory);
+    event TransactionDeposited(address indexed from, address indexed to, uint256 indexed nonce, bytes data);
+    event OmniBlockAdded(uint256 indexed blockNumber, OmniCodec.BlockChunk block);
+    event XChainTxResult(bytes32 indexed sourceTxHash, uint256 indexed omniNonce, bool success);
 
-    // messages
-    function sendOmniTx(address _to, bytes memory _data) external payable;
+    function chain() external view returns (string memory);
+    function isOrchestrator(address _address) external view returns (bool);
+
+    // blocks
+    function getLatestOmniBlockChunk() external view returns (OmniCodec.BlockChunk memory);
+    function addOmniBlockChunk(OmniCodec.BlockChunk calldata _blockChunk, bytes[] calldata signatures) external;
+    function addOmniBlockChunks(OmniCodec.BlockChunk[] calldata _blockChunks, bytes[][] calldata signatures) external;
+
+    // outbound tx
+    function sendOmniTx(address _to, bytes memory _data) external;
     function sendXChainTx(string memory _chain, address _to, bytes memory _data) external;
+    function nonce() external view returns (uint256);
+
+    // inbound tx metadata
+    function txSender() external view returns (address); // tx.from if tx is from omni
+    function txSourceChain() external returns (string memory);
+    function isXChainTx() external view returns (bool); // true if tx is a XChainTx
+    function isOmniTx() external view returns (bool);
+    function isTxFrom(string memory) external view returns (bool);
+    function isTxFromOneOf(string memory, string memory) external view returns (bool);
+    function isTxFromOneOf(string memory, string memory, string memory) external view returns (bool);
+    function isTxFromOneOf(string memory, string memory, string memory, string memory) external view returns (bool);
+    function isTxFromOneOf(string memory, string memory, string memory, string memory, string memory)
+        external
+        view
+        returns (bool);
 
     // state verification
     function verifyOmniState(
@@ -23,4 +42,11 @@ interface IOmniPortal {
         bytes memory _storageKey,
         bytes memory _storageValue
     ) external view returns (bool);
+
+    // admin
+    function addSupportedChain(string memory _chain) external;
+    function removeSupportedChain(string memory _chain) external;
+    function addOrchestrator(address _orchestrator) external;
+    function removeOrchestrator(address _orchestrator) external;
+    function resetState(OmniCodec.BlockChunk calldata _block, uint256 _nonce) external;
 }
